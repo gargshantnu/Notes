2bd784d8c17a3abd9934f289997be4bf51ad3fe2
Shantnu Garg <gargshantnu@gmail.com>
2022-02-27 23:02:26 +0530

    add dummy functions for all notes operations

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ console.log("Starting app");
 
 const fs = require("fs");
 const _ = require("lodash");
+const yargs = require("yargs");
 
 const notes = require("./notes.js");
 
 // console.log(process.argv);
 
-const command = process.argv[2];
+const command = yargs._[0];
+const argv = yargs.argv;
 
 if (command == "add") {
     console.log("Adding new note");
+    console.log(argv.title, argv.body);
+    notes.addNote(argv.title, argv.body);
 } else if (command == "list") {
     console.log("Listing note");
+    notes.getAllNotes();
 } else if (command == "read") {
     console.log("Reading note");
+    notes.getNoteByTitle(argv.title);
 } else if (command == "remove") {
     console.log("Removing note");
 } else {
--- notes.js
+++ notes.js
@@ -1,10 +1,26 @@
 console.log("Starting app");
 
-module.exports.addNote = () => {
-    console.log("Adding note");
-    return "New Note";
+
+
+const addNote = (title, body) => {
+    console.log("Adding note", title, body);
 }
 
-module.exports.add = (a, b) => {
-    return a + b;
+const getAllNotes = () => {
+    console.log("Getting all note");
+}
+
+const getNoteByTitle = (title) => {
+    console.log("Getting single note by title: ", title);
+}
+
+const removeNoteByTitle = (title) => {
+    console.log("Removing note by title: ", title);
+}
+
+module.exports = {
+    addNote,
+    getAllNotes,
+    getNoteByTitle,
+    removeNoteByTitle,
 }
