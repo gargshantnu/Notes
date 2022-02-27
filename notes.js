12c77bfc4d25291feafa540f002bb7945f31497d
Shantnu Garg <gargshantnu@gmail.com>
2022-02-27 21:52:09 +0530

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log("Starting app");
+
+module.exports.addNote = () => {
+    console.log("Adding note");
+    return "New Note";
+}
+
+module.exports.add = (a, b) => {
+    return a + b;
+}
